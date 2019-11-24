68728c486c4e7a7019af2c2d643b2d428b0dc760
91hongppie <91hongppie@gmail.com>
2019-11-24 17:27:21 +0900

    feature/indexhtml

--- movies/views.py
+++ movies/views.py
@@ -3,6 +3,12 @@ from .models import Movie
 import json
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    context = {'movies': movies,}
+    return render(request, 'movies/index.html', context)
+
+
 def detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)   
     movie.actors = json.loads(movie.actors.replace("'", "\""))
