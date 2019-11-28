a03cf562b7293a68facff875b4eb6cf920efebaf
Gagle637 <gagle637@gmail.com>
2019-11-28 19:00:53 +0900

    qq

--- movies/views.py
+++ movies/views.py
@@ -94,6 +94,7 @@ def like(request, movie_pk):
     else:
         return HttpResponseBadRequest()
 
+
 def genre_movie(request, genre_nm):
     movies = Movie.objects.all()
     genre_list = ['전체보기', '액션', '모험', '애니메이션', '코미디', '범죄', '다큐멘터리', '드라마', '가족', '판타지', '역사', '공포', '음악', '미스터리', '로맨스', 'SF', 'TV 영화', '스릴러', '전쟁', '서부']
